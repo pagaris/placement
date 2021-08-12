b2980e5b685c46fbec408a5bd450999f5de5b7e1
Daniel Olivares <daniel@inticlabs.com>
2021-08-12 17:18:05 -0500

    Move modals to separate <pagaris-modal> elements, appended to doc. body
    This makes the modals work when <pagaris-placement> placement prevented 
    the modal from showing correctly.

--- src/pagaris_placement.js
+++ src/pagaris_placement.js
@@ -1,3 +1,7 @@
+/**
+ * Custom Element in charge of fetching pagaris.com/placement and dividing the
+ * response into a modal and a link/button (in charge of opening the modal).
+ */
 class PagarisPlacement extends HTMLElement {
   static get observedAttributes() {
     return ['amount', 'ref', 'format', 'skip-modal']
@@ -27,43 +31,75 @@ class PagarisPlacement extends HTMLElement {
     const url = baseUrl + params.toString()
 
     fetch(url).then(response => response.text()).then(html => {
-      // Insert HTML response to Shadow DOM
+      // Insert HTML response to Shadow DOMs (from link and modal elements)
       const parser = new DOMParser()
-      const doc = parser.parseFromString(html, 'text/html')
+
+      // Link document. Modal will be removed here and it will be applied to
+      // this element's shadow DOM.
+      const linkDoc = parser.parseFromString(html, 'text/html')
+
+      let modalDoc, modalEl
+      if (!this._skipModal) {
+        const modal = linkDoc.querySelector('#details-modal')
+        modal?.remove()
+
+        // Modal doc. Link will be removed here and this document will be
+        // applied to a pagaris-modal element that will be appended to body.
+        modalDoc = parser.parseFromString(html, 'text/html')
+        const link = modalDoc.querySelector('#modal-link')
+        link?.remove()
+
+        modalEl = document.createElement('pagaris-modal')
+        document.body.appendChild(modalEl)
+      }
+
       const shadow = this.shadowRoot || this.attachShadow({ mode: 'open' })
       shadow.innerHTML = ''
-      shadow.append(doc.querySelector('html'))
+      shadow.append(linkDoc.querySelector('html'))
+
+      let modalShadow
+      if (modalDoc) {
+        modalShadow = modalEl.shadowRoot ||
+          modalEl.attachShadow({ mode: 'open' })
+        modalShadow.innerHTML = ''
+        modalShadow.append(modalDoc.querySelector('html'))
+      }
 
       // (Re)insert scripts to Shadow DOM, so that they are evaluated
       const scriptsSelector = 'script[src], script[data-insert-to-shadow]'
-      shadow.querySelectorAll(scriptsSelector).forEach((item, i) => {
-        const script = document.createElement('script')
-        if (item.src) script.src = item.src
-        if (item.innerHTML.length) script.innerHTML = item.innerHTML
-        shadow.querySelector('head').appendChild(script)
-      })
+      const reinsertScripts = (shadow) => {
+        shadow.querySelectorAll(scriptsSelector).forEach((item, i) => {
+          const script = document.createElement('script')
+          if (item.src) script.src = item.src
+          if (item.innerHTML.length) script.innerHTML = item.innerHTML
+          shadow.querySelector('head').appendChild(script)
+        })
+      }
+      reinsertScripts(shadow)
+      if (modalShadow) reinsertScripts(modalShadow)
 
       // Fix modal: Trigger an event when toggling button is clicked, which
       // will be catched in source (Pagaris).
       const button = shadow.querySelector('button[data-toggle=modal], a#modal-link')
-      if (button) {
+      if (button && modalEl) {
         button.addEventListener('click', () => {
           document.dispatchEvent(new CustomEvent('placementButtonClick', {
-            detail: { shadow, ref: this._ref }
+            detail: { shadow: modalShadow, ref: this._ref }
           }))
 
           // Move backrop to shadow root instead of parent DOM
-          const backdrop = document.querySelector('body > .modal-backdrop')
-          if (backdrop) {
-            backdrop.parentNode.removeChild(backdrop)
-            shadow.appendChild(backdrop)
+          const backdrops = document.querySelectorAll('body > .modal-backdrop')
+          if (backdrops.length) {
+            backdrops.forEach(item => item.remove())
+
+            modalShadow.appendChild(backdrops[0])
           }
 
           // Remove .modal-open from parent body and add to shadow body
           const body = document.querySelector('body.modal-open')
           if (body) {
             body.classList.remove('modal-open')
-            shadow.querySelector('body')?.classList.add('modal-open')
+            modalShadow.querySelector('body')?.classList.add('modal-open')
           }
         })
       }
